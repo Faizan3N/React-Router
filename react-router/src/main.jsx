272b5e92ccadf0c395b289bdd57ce6cac03789db
Faizan <faizanali3n@gmail.com>
2025-09-02 19:13:02 +0500

    Header&Footer added

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
+
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider ,createBrowserRouter } from 'react-router-dom'
+
+import Layout from './Layout.jsx';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        
+          path: "",
+          element: <Home/>
+      },
+          {}
+      ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
